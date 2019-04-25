f8da7e663d6b4a9a49a6c96a61ab6cd13fce429a
BACCI Ange <b18001070@a-maq-iutinfo.salsa.univ-amu.fr>
2019-04-25 14:19:05 +0200

    Exercice 1

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -2,6 +2,6 @@ package fr.univ_amu.iut.exercice1;
 
 public class HelloWorld {
     public static String hello(String s) {
-        throw new RuntimeException("Not yet implemented !");
+        return String.format("Hello, %s!", s);
     }
 }
